4068574951341fa448038126ae9f8a37e71aa4a7
chobeat <simone.robutti@gmail.com>
2017-05-22 21:11:37 +0200

    Build and start script

---
+++ start_notebook.sh
@@ -0,0 +1,9 @@
+if [ $# -eq 0 ]
+	then
+	
+		echo "No arguments supplied."
+		echo "Usage: ./start_notebook.sh mount_dir"
+		exit 1
+fi
+
+docker run -it -v $1:/home/jovyan/work --rm -p 8888:8888 data-doctor-notebook start-notebook.sh --NotebookApp.base_url=/home/jovyan/
